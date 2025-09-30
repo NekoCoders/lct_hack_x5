753b67614786e93128a30741c9692e17f093ab67
dreyk111 <lotusbro>
2025-09-30 21:42:20 +0300

    fix recognizing in server

--- server/model_runner.py
+++ server/model_runner.py
@@ -1,5 +1,6 @@
 
 import logging
+
 log = logging.getLogger("model_runner")
 
 log.info("Start of loading transformers library")
@@ -16,6 +17,7 @@ if PROJECT_ROOT not in sys.path:
     sys.path.insert(0, PROJECT_ROOT)
 # -------------
 from model.interface import SpanType
+from server.interface import Entity
 from model.postprocessing import splitted_bio_spans_from_ents
 
 MODEL_ID = "lotusbro/x5-ner"
@@ -31,19 +33,22 @@ def load_model():  # FIXME: must be in module "model"?
     return pipe
 
 
-def infer_model(texts: list[str]) -> list[SpanType]:  # FIXME: must be in module "model"?
+def infer_model(texts: list[str]) -> list[list[Entity]]:  # FIXME: must be in module "model"?
     global pipe
     if "pipe" not in globals():
         pipe = load_model()
 
     result_ents_for_texts = pipe(texts)
-    result_spans_for_texts = []
+    response_entities_for_texts = []
     for i, result_ents in enumerate(result_ents_for_texts):
         result_spans_for_text = splitted_bio_spans_from_ents(text=texts[i], ents=result_ents, model_id=MODEL_ID)
-        result_spans_for_texts.append(result_spans_for_text)
-    return result_spans_for_texts
+        response_entities = [
+                                Entity(start_index=start, end_index=end, entity=label)
+                             for start, end, label in result_spans_for_text]
+        response_entities_for_texts.append(result_spans_for_text)
+    return response_entities_for_texts
 
 
 if __name__ == "__main__":
-    result = infer_model("Молоко простоквашино")
+    result = infer_model(["Молоко простоквашино"])
     print(result)
